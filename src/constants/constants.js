81737923a08293cfb7f75e839a1762d0ac56a5e6
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-05 18:11:07 +0500

    refactor: add commands enum like object

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,12 @@
 import { ListCommand, UpCommand } from '../commands/index.js';
 
+const commands = {
+  up: 'up',
+  ls: 'ls',
+};
+
 const commandsMap = new Map();
-commandsMap.set('up', new UpCommand());
-commandsMap.set('ls', new ListCommand());
+commandsMap.set(commands.up, new UpCommand());
+commandsMap.set(commands.ls, new ListCommand());
 
 export { commandsMap };
