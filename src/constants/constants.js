02f3cdc069f5beb84ca3b0af619ebf0c305af656
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-10-05 22:02:58 +0500

    refactor: extract osCommandsMap

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -18,4 +18,37 @@ commandsMap.set(commands.ls, new ListCommand());
 commandsMap.set(commands.cd, new ChangeDirectoryCommand());
 commandsMap.set(commands.os, new OperatingSystemCommand());
 
-export { commandsMap };
+const osCommandArgs = {
+  eol: '--EOL',
+  cpus: '--cpus',
+  homedir: '--homedir',
+  username: '--username',
+  architecture: '--architecture',
+};
+
+const osCommandsMap = new Map();
+
+osCommandsMap.set(osCommandArgs.eol, (osModule, loggerService) => {
+  loggerService.logMessage(osModule.EOL);
+});
+
+osCommandsMap.set(osCommandArgs.cpus, (osModule, loggerService) => {
+  const cpusInfo = osModule.cpus();
+  const cpusData = cpusInfo.map((cpu) => cpu.model);
+  console.log(cpusData);
+  loggerService.logMessage(`TOTAL CPUS: ${osModule.cpus().length}`);
+});
+
+osCommandsMap.set(osCommandArgs.homedir, (osModule, loggerService) => {
+  loggerService.logMessage(osModule.homedir());
+});
+
+osCommandsMap.set(osCommandArgs.username, (osModule, loggerService) => {
+  loggerService.logMessage(osModule.hostname());
+});
+
+osCommandsMap.set(osCommandArgs.architecture, (osModule, loggerService) => {
+  loggerService.logMessage(osModule.arch());
+});
+
+export { commandsMap, osCommandsMap };
